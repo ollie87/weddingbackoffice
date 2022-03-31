b4976ddc37f988fc1a7bd29db51af9aaa0948b19
Mario Garrido <>
2022-03-31 10:01:06 +0200

    feat(GuestReducer): fix error

--- src/adapters/contexts/GuestReducer.ts
+++ src/adapters/contexts/GuestReducer.ts
@@ -4,10 +4,12 @@ interface iActionReducer {
     type: string,
     payload: any
 }
-export default (state: any, action: iActionReducer) => {
+const GuestReducer = (state: any, action: iActionReducer) => {
     switch (action.type) {
         case GET_GUESTS: {
             return { ...state, guestList: action.payload }
         }
     }
 }
+
+export default GuestReducer;
